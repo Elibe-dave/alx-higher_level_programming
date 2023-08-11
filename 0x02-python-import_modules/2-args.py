e7ca81c121b8d6e7c6fd956807c66e76d5f72f6c
Elibe-dave <elibe.dave@gmail.com>
2023-08-11 12:47:00 -0700

    refactored task2 with corrwct logic

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+    """Print the number of and list of arguments."""
     import sys
 
-    argv = sys.argv[1:]
-    num_args = len(argv)
-
-    if num_args == 0:
-        print("Number of argument(s): 0.")
-        print("No arguments.")
-    elif num_args == 1:
-        print("Number of argument(s): 1.")
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
         print("1 argument:")
     else:
-        print("Number of argument(s): {}.".format(num_args))
-        print("{} arguments:".format(num_args))
-
-    for idx, arg in enumerate(argv, start=1):
-        print("{}: {}".format(idx, arg))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
 
