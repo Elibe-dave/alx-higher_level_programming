3feca49c2228c363bdcb955ba046991dc0273f92
Elibe-dave <elibe.dave@gmail.com>
2023-08-15 12:55:51 -0700

    updated task8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
 
-    """Find the biggest integer of a list."""
 
-    if len(my_list) == 0:
+def multiple_returns(sentence):
 
-        return (None)
-    big = my_list[0]
+    """Returns the length of a string and its first character."""
 
-    for i in range(len(my_list)):
+    if sentence == "":
 
-        if my_list[i] > big:
+        return (0, None)
 
-            big = my_list[i]
-    return (big)
+    return (len(sentence), sentence[0])i
